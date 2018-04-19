53ef62f68bb6358cbb8471449b53c507b9bad486
Pete Goodwin <pgoodwin@imekon.org>
2018-04-19 08:22:25 +0100

    add Quad Panner

--- src/Template.cpp
+++ src/Template.cpp
@@ -13,6 +13,7 @@ void init(Plugin *p)
 	// Add all Models defined throughout the plugin
 	p->addModel(modelPGSEQ3);
     p->addModel(modelPGPanner);
+    p->addModel(modelPGQuadPanner);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
